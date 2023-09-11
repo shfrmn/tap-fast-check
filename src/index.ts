44eb7b0a5c4cce922e7ccb3a510b655b0554e18d
Joseph Shaferman <joseph.shaferman@gmail.com>
2023-09-11 14:23:09 +0800

    feat: prop message & assert parameters

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,38 @@ export {t, fc}
 declare global {
   export namespace Tap {
     export interface Test {
-      prop<T>(this: Tap.Test, prop: fc.IProperty<T>, message: string): boolean
+      prop<T>(
+        this: Tap.Test,
+        prop: fc.IProperty<T>,
+        paramsOrMessage1?: fc.Parameters<T> | string,
+        paramsOrMessage2?: fc.Parameters<T> | string,
+      ): boolean
     }
   }
 }
 
-t.Test.prototype.addAssert("prop", 1, function <
+t.Test.prototype.addAssert("prop", 3, function <
   T,
->(this: Tap.Test, prop: fc.IProperty<T>, message: string) {
-  const {counterexample} = fc.check(prop)
-  return this.strictSame(null, counterexample, message)
+>(this: Tap.Test, prop: fc.IProperty<T>, paramsOrMessage1?: fc.Parameters<T> | string, paramsOrMessage2?: fc.Parameters<T> | string): boolean {
+  const params = [paramsOrMessage1, paramsOrMessage2].find(
+    (arg): arg is fc.Parameters<T> => typeof arg === "object",
+  )
+  const propMessage = [paramsOrMessage1, paramsOrMessage2].find(
+    (arg): arg is string => typeof arg === "string",
+  )
+  const {counterexample, failed, error} = fc.check(prop, {
+    ...(params || {}),
+    endOnFailure: true,
+  })
+  if (failed && counterexample) {
+    return this.strictSame(
+      null,
+      counterexample,
+      `${propMessage}: failed with inputs`,
+    )
+  } else if (failed) {
+    return this.notOk(error, `${propMessage}: interrupted or too many skips`)
+  } else {
+    return this.pass(propMessage)
+  }
 })
